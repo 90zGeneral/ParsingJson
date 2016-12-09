3126decd5eb796d4548d602b3b09d12a17d9ec89
Roydon Jeffrey <brushdam14823@gmail.com>
2016-12-08 20:19:32 -0500

    Making API calls while working with closures. Also added break points to see the order in which random lines execute

--- FetchData/ViewController.swift
+++ FetchData/ViewController.swift
@@ -13,13 +13,57 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        fetchData() { result in
+            print(result?.timeStamp ?? "not working")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    struct Coordinate {        
+        var lat: String
+        var long: String
     }
-
+    
+    class Position {
+        var timeStamp: Int
+        var coordinate: Coordinate
+        
+        init(jsonOject: [String: Any]) {
+            
+            self.timeStamp = jsonOject["timestamp"] as? Int ?? 0
+            let position = jsonOject["iss_position"] as? [String: Any]
+            let lat = position?["latitude"] as? String ?? "unknown"
+            let long = position?["longitude"] as? String ?? "unknown"
+            self.coordinate = Coordinate(lat: lat, long: long)
+        }
+    }
+    
+    func parseJSON(data: Data, completionHandler1: @escaping (Position?)->()) {
+        var position: Position? = nil
+        if let jsonObject = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [String: Any] {
+            position = Position(jsonOject: jsonObject)
+        }
+        
+        if !Thread.isMainThread {
+            DispatchQueue.main.async {
+                completionHandler1(position)
+            }
+        }
+    }
+    
+    func fetchData(completionHandler2: @escaping (Position?)->()) {
+        let url = URL(string: "http://api.open-notify.org/iss-now.json")!
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            guard let responseData = data else {
+                completionHandler2(nil)
+                return
+            }
+            
+            print(Thread.isMainThread)
+            
+            self.parseJSON(data: responseData, completionHandler1: completionHandler2)
+            }.resume()
+    }
 
 }
 
